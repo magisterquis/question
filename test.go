565098524254125aecaab378df1b3b26ba0a9c9c
Stuart <kd5pbo@gmail.com>
2018-01-03 11:21:10 -0500

    dev

--- test.go
+++ test.go
@@ -1,18 +1,32 @@
 package main
 
+import (
+	"log"
+
+	"golang.org/x/crypto/ssh"
+)
+
 const (
 	USERNAME = "admin"
-	PASSWORD="password123"
-	SERVER="10.3.2.4:22"
+	PASSWORD = "password123"
+	SERVER   = "10.3.2.4:22"
 )
-func main(){
-	ssh.Dial(
+
+func main() {
+	c, _ := ssh.Dial(
 		"tcp",
 		SERVER,
 		&ssh.ClientConfig{
-			User:USERNAME,
-			Auth:[]ssh.AuthMethod{
+			User: USERNAME,
+			Auth: []ssh.AuthMethod{
 				ssh.Password(PASSWORD),
 			},
+			HostKeyCallback: ssh.InsecureIgnoreHostKey,
 		},
-		HostKeyCallback: ssh.InsecureIgnoreHostKey,
+	)
+	if nil != c {
+		log.Printf("Up")
+	} else {
+		log.Printf("Down")
+	}
+}
